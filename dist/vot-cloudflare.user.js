ccd644aa887e6290593f35b518f62701dd92aeeb
Pozhydaiev Serhii <abagour@gmail.com>
2023-09-28 16:22:48 +0300

    Update vot-cloudflare.user.js

--- dist/vot-cloudflare.user.js
+++ dist/vot-cloudflare.user.js
@@ -909,7 +909,7 @@ const sitesProxyTok = [
 /* harmony export */   i: () => (/* binding */ workerHost)
 /* harmony export */ });
 // CLOUDFLARE CONFIGURATION
-const workerHost = "vot-new.toil-dump.workers.dev";
+const workerHost = "worker-fragrant-hat-a6db.skalarua.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 
 
@@ -926,7 +926,7 @@ const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 /* harmony export */ });
 /* unused harmony exports workerHost, yandexHmacKey */
 // CONFIGURATION
-const workerHost = "api.browser.yandex.ru";
+const workerHost = "worker-fragrant-hat-a6db.skalarua.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 const yandexUserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
